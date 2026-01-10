0703fa64b7ae17e869d8c3b94d0a6801664b5fb4
abhinathtiwari <abhinathtiwari1@gmail.com>
2026-01-10 15:32:17 +0530

    [modify] delete user account api This api deletes user's account and revokes permissions of channel access

--- backend/src/routes/authUser.routes.ts
+++ backend/src/routes/authUser.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { User } from "../models/Users";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { authMiddleware } from "../utils/middlewares";
+import axios from "axios";
 
 const router = Router();
 
@@ -48,6 +50,49 @@ router.post("/login", async (req, res) => {
     secure: false      
     });
   res.json({ message: "Login successful" });
+});
+
+//delete user's account
+router.delete("/deleteaccount", authMiddleware, async (req: any, res) => {
+  const user = req.user;
+
+  if (!user) {
+    return res.status(401).json({
+      message: "User not authenticated",
+    });
+  }
+
+  try {
+    const result = await User.deleteOne({ email: user.email });
+    if (user.youtubeRefreshToken) {
+      const res = await axios.post(
+        "https://oauth2.googleapis.com/revoke",
+        null,
+        {
+          params: {
+            token: user.youtubeRefreshToken,
+          },
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+    }
+    if (result.deletedCount === 0) {
+      return res.status(404).json({
+        message: `Account for ${user.email} was not found`,
+      });
+    }
+    return res.status(200).json({
+      message: `Account for ${user.email} has been deleted! All YouTube permissions have been revoked.
+      ${res}
+      `,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: `Account for ${user.email} was not completely deleted`,
+    });
+  }
 });
 
 export default router;
