fb968dc604bed03c6986d950e4795a624a53c82c
abhinathtiwari <abhinathtiwari1@gmail.com>
2026-01-10 15:34:45 +0530

    [modify] delete button added delete button which goes back to auth page "/"

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,13 +1,29 @@
 import { useEffect } from "react";
+import Layout from "../components/Layout";
+import api from "../api";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Settings() {
-
+  const navigate = useNavigate();
+  
+  async function deleteAccount(){
+    try{
+      const url = "/auth/user/deleteaccount";
+      await api.delete(url);
+      navigate("/");
+    }catch(err){
+      console.log(err);
+    }
+  }
 
   useEffect(() => {
   }, []);
 
 
   return (
-     <h1>Settings</h1>
+    <Layout>
+      <h1>Settings</h1>
+      <button onClick={()=>{deleteAccount()}}>Delete Account</button>
+    </Layout>
   );
 }
